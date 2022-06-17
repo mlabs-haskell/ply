afbe42b5d82dda3683ab88fd61918896cd9e545e
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-06-17 17:18:37 +0530

    Fix formatting and update

--- example/offchain/app/Main.hs
+++ example/offchain/app/Main.hs
@@ -1,19 +1,38 @@
 module Main (main) where
 
-import Control.Monad.Reader
-import Data.Fixed (Micro, Fixed (MkFixed))
-import Data.Default (Default(def))
-import System.FilePath ( (</>) )
+import Control.Monad.Reader (ReaderT, ask)
+import Data.Default (Default (def))
+import Data.Fixed (Fixed (MkFixed), Micro)
+import System.FilePath ((</>))
 
-import Test.Plutip.Internal.Types
-import Test.Plutip.LocalCluster
-import Test.Plutip.Internal.BotPlutusInterface.Wallet (walletPkh)
 import Test.Plutip.Internal.BotPlutusInterface.Run (runContract)
+import Test.Plutip.Internal.BotPlutusInterface.Wallet (walletPkh)
+import Test.Plutip.Internal.Types (
+  ClusterEnv,
+  ExecutionResult (ExecutionResult),
+  FailureReason (CaughtException, ContractExecutionError),
+ )
+import Test.Plutip.LocalCluster (
+  BpiWallet,
+  addSomeWallet,
+  startCluster,
+  stopCluster,
+  waitSeconds,
+ )
 
-import Ledger (PaymentPubKeyHash(PaymentPubKeyHash))
+import Ledger (PaymentPubKeyHash (PaymentPubKeyHash))
 import Ply (readTypedScript)
 
-import Example.Nft
+import Example.Nft (
+  MintParams (
+    MintParams,
+    mpDescription,
+    mpName,
+    mpPubKeyHash,
+    mpTokenName
+  ),
+  mintNft,
+ )
 
 main :: IO ()
 main = do
@@ -22,30 +41,31 @@ main = do
   nftMp <- readTypedScript $ "../" </> "compiled" </> "nftMp.plutus"
 
   -- Do stuff.
-  ExecutionResult exOutcome _ _ _ <- runContract cEnv ownWallet
-    $ mintNft nftMp
-    $ MintParams
-      { mpName="exampleNFT"
-      , mpDescription=Nothing
-      , mpTokenName="NFTA"
-      , mpPubKeyHash=PaymentPubKeyHash $ walletPkh ownWallet
-      }
+  ExecutionResult exOutcome _ _ _ <-
+    runContract cEnv ownWallet $
+      mintNft nftMp $
+        MintParams
+          { mpName = "exampleNFT"
+          , mpDescription = Nothing
+          , mpTokenName = "NFTA"
+          , mpPubKeyHash = PaymentPubKeyHash $ walletPkh ownWallet
+          }
   case exOutcome of
-      Left (ContractExecutionError e) -> putStrLn "Contract failed" >> print e
-      Left (CaughtException e) -> putStrLn "Unexpected exception" >> print e
-      Right _ -> putStrLn "Contract ran successfully"
+    Left (ContractExecutionError e) -> putStrLn "Contract failed" >> print e
+    Left (CaughtException e) -> putStrLn "Unexpected exception" >> print e
+    Right _ -> putStrLn "Contract ran successfully"
 
   -- Stop the node.
   stopCluster clusterStat
   where
     setup :: ReaderT ClusterEnv IO (ClusterEnv, BpiWallet)
     setup = do
-        env <- ask
-        -- Gotta have all those utxos for the collaterals.
-        ownWallet <- addWalletWithAdas $ 100 : replicate 20 10
-        -- Wait for faucet funds to be added.
-        waitSeconds 2
-        pure (env, ownWallet)
+      env <- ask
+      -- Gotta have all those utxos for the collaterals.
+      ownWallet <- addWalletWithAdas $ 100 : replicate 20 10
+      -- Wait for faucet funds to be added.
+      waitSeconds 2
+      pure (env, ownWallet)
 
     addWalletWithAdas :: [Ada] -> ReaderT ClusterEnv IO BpiWallet
     addWalletWithAdas = addSomeWallet . map (fromInteger . toLovelace)
