8e3ba0d83192fef68f7e82ee6fd888572b3b240a
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-06-22 16:53:34 +0530

    Export script serialization helpers

--- ply-core/src/Ply/Core/Serialize.hs
+++ ply-core/src/Ply/Core/Serialize.hs
@@ -1,10 +1,13 @@
-module Ply.Core.Serialize (writeEnvelope) where
+module Ply.Core.Serialize (writeEnvelope, serializeScript, serializeScriptCbor, serializeScriptCborHex) where
 
 import Codec.Serialise (serialise)
 import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Short as SBS
 import Data.Text (Text)
+import qualified Data.Text.Encoding as Txt
 
 import qualified Cardano.Binary as CBOR
 import Plutus.V1.Ledger.Scripts (Script)
@@ -15,6 +18,18 @@ import Ply.Core.Types (
   TypedScriptEnvelope' (..),
   Typename,
  )
+
+-- | Serialize a script into its raw form.
+serializeScript :: Script -> ByteString
+serializeScript = LBS.toStrict . serialise
+
+-- | Serialize a script into CBOR.
+serializeScriptCbor :: Script -> ByteString
+serializeScriptCbor = CBOR.serialize' . SBS.toShort . serializeScript
+
+-- | A showable hex string representing a serialized script in CBOR.
+serializeScriptCborHex :: Script -> Text
+serializeScriptCborHex = Txt.decodeUtf8 . Base16.encode . serializeScriptCbor
 
 -- | Write a typed script into a 'TypedScriptEnvelope'.
 writeEnvelope ::
@@ -30,16 +45,14 @@ writeEnvelope ::
   Script ->
   IO ()
 writeEnvelope descr filepath scrptRole paramTypes scrpt = do
-  let scriptRaw = LBS.toStrict . serialise $ scrpt
-      scriptRawCBOR = CBOR.serialize' . SBS.toShort $ scriptRaw
-      plutusEnvelope =
+  let plutusEnvelope =
         TypedScriptEnvelope'
           { tsVersion' = ScriptV1
           , tsRole' = scrptRole
           , tsParamTypes' = paramTypes
           , tsDescription' = descr
-          , tsCbor' = scriptRawCBOR
-          , tsRaw' = scriptRaw
+          , tsCbor' = serializeScriptCbor scrpt
+          , tsRaw' = serializeScript scrpt
           }
       content = encodePretty plutusEnvelope
   LBS.writeFile filepath content
