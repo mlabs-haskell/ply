b508cd06dff931479a926f5275ef1cf43e277b0e
Seungheon Oh <seungheon.ooh@gmail.com>
2022-09-27 20:43:06 -0500

    Dropped `TypedScriptEnvelope'`
    `TypedScriptEnvelope` now gets Aeson instances

--- ply-core/src/Ply/Core/Serialize.hs
+++ ply-core/src/Ply/Core/Serialize.hs
@@ -1,38 +1,23 @@
 module Ply.Core.Serialize (
   writeEnvelope,
   writeTypedScriptEnvolope,
-  serializeScript,
-  serializeScriptCbor,
   serializeScriptCborHex,
 ) where
 
-import Codec.Serialise (serialise)
 import Data.Aeson.Encode.Pretty (encodePretty)
-import Data.ByteString (ByteString)
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Short as SBS
 import Data.Text (Text)
 import qualified Data.Text.Encoding as Txt
-
-import qualified Cardano.Binary as CBOR
 
 import Ply.Core.Types (
   ScriptRole,
   ScriptVersion,
   TypedScriptEnvelope (..),
-  TypedScriptEnvelope' (..),
   Typename,
+  serializeScriptCbor,
  )
 import Ply.LedgerExports.Common (Script)
-
--- | Serialize a script into its raw form.
-serializeScript :: Script -> ByteString
-serializeScript = LBS.toStrict . serialise
-
--- | Serialize a script into CBOR.
-serializeScriptCbor :: Script -> ByteString
-serializeScriptCbor = CBOR.serialize' . SBS.toShort . serializeScript
 
 -- | A showable hex string representing a serialized script in CBOR.
 serializeScriptCborHex :: Script -> Text
@@ -40,8 +25,8 @@ serializeScriptCborHex = Txt.decodeUtf8 . Base16.encode . serializeScriptCbor
 
 -- | Write 'TypedScriptEnvelop' into file.
 writeTypedScriptEnvolope :: FilePath -> TypedScriptEnvelope -> IO ()
-writeTypedScriptEnvolope filepath (TypedScriptEnvelope ver role param descr script) =
-  writeEnvelope descr filepath ver role param script
+writeTypedScriptEnvolope filepath =
+  LBS.writeFile filepath . encodePretty
 
 -- | Write a typed script into a "Ply.Core.Types.TypedScriptEnvelope".
 writeEnvelope ::
@@ -58,15 +43,7 @@ writeEnvelope ::
   -- | The script itself.
   Script ->
   IO ()
-writeEnvelope descr filepath scrptVer scrptRole paramTypes scrpt = do
+writeEnvelope descr filepath scrptVer scrptRole paramTypes scrpt =
   let plutusEnvelope =
-        TypedScriptEnvelope'
-          { tsVersion' = scrptVer
-          , tsRole' = scrptRole
-          , tsParamTypes' = paramTypes
-          , tsDescription' = descr
-          , tsCbor' = serializeScriptCbor scrpt
-          , tsRaw' = serializeScript scrpt
-          }
-      content = encodePretty plutusEnvelope
-  LBS.writeFile filepath content
+        TypedScriptEnvelope scrptVer scrptRole paramTypes descr scrpt
+   in writeTypedScriptEnvolope filepath plutusEnvelope
