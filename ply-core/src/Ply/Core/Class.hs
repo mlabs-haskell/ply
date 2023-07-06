99c1cc18714ed35de74c1f91a2ec2d3074cd2e2f
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2023-07-06 14:59:44 -0600

    Initial schema design

--- ply-core/src/Ply/Core/Class.hs
+++ ply-core/src/Ply/Core/Class.hs
@@ -1,36 +1,29 @@
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Ply.Core.Class (PlyArg (UPLCRep, ToDataConstraint, toBuiltinArg, toBuiltinArgData), someBuiltinArg) where
+module Ply.Core.Class (PlyArg (..), type ToDataConstraint, someBuiltinArg, someBuiltinArgData) where
 
-import Data.Bifunctor (Bifunctor (bimap), second)
 import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import Data.Kind (Constraint, Type)
-import qualified Data.Map.Strict as M
+import Data.Kind (Constraint)
 import Data.Text (Text)
 import GHC.TypeLits (ErrorMessage (ShowType, Text))
-import Ply.Core.Types (AsData (AsData))
 
-import PlutusCore (DefaultUni, HasTermLevel, Some, ValueOf)
+import Data.SOP
+import PlutusCore (DefaultUni, Some, ValueOf)
+import qualified GHC.TypeLits as TLits
 import qualified PlutusCore as PLC
-import qualified PlutusTx.AssocMap as PlutusMap
-
 import PlutusLedgerApi.V1 as LedgerCommon hiding (
   ScriptContext (..),
   TxInInfo (..),
   TxInfo (..),
   TxOut,
  )
-import qualified PlutusLedgerApi.V1 as LedgerV1
-import PlutusLedgerApi.V1.Time as LedgerCommon (
-  DiffMilliSeconds (DiffMilliSeconds),
- )
-import PlutusLedgerApi.V1.Value as LedgerCommon (
-  AssetClass (AssetClass),
- )
+import qualified PlutusTx.Builtins as PlutusTx
 import qualified PlutusTx.Ratio as PlutusTx
+
+import Ply.Core.Schema
 
 type BottomConstraint s t = 'Text s ~ ShowType t
 
@@ -53,357 +46,88 @@ As a result, 'toBuiltinArg' is partial for types that don't hold their invariant
 
  4. 'toBuiltinArgData' implementation must perform the same validations/normalizations as 'toBuiltinArg'.
 -}
-class DefaultUni `HasTermLevel` UPLCRep a => PlyArg a where
-  type UPLCRep a :: Type
-  type ToDataConstraint a :: Constraint
-  type ToDataConstraint a = ()
-  toBuiltinArg :: a -> UPLCRep a
-  toBuiltinArgData :: ToDataConstraint a => a -> Data
+class (HasSchemaInstance (UPLCSchema a), ToPLCDefaultUni (UPLCSchema a)) => PlyArg a where
+  type UPLCDataSchema a :: PlyDataSchema
+  type UPLCSchema a :: PlySchema
+  type UPLCSchema a = PlyD (UPLCDataSchema a)
+  type ToDataConstraint' a :: Constraint
+  type ToDataConstraint' a = ()
+  toBuiltinArg :: a -> SchemaInstanceOf (UPLCSchema a)
+  default toBuiltinArg :: (ToDataConstraint' a, UPLCSchema a ~ PlyD (UPLCDataSchema a)) => a -> SchemaInstanceOf (UPLCSchema a)
+  toBuiltinArg = getDataSchemaInstanceOf . toBuiltinArgData
+  toBuiltinArgData :: ToDataConstraint' a => a -> DataSchemaInstanceOf (UPLCDataSchema a)
+
+type ToDataConstraint a = (ToDataConstraint' a, ToPLCDefaultUni ( 'PlyD (UPLCDataSchema a)), DefaultUniTarget ( 'PlyD (UPLCDataSchema a)) ~ Data)
 
 -- | Create a PlutusCore 'Some' value from a 'PlyArg'.
 someBuiltinArg :: PlyArg a => a -> Some (ValueOf DefaultUni)
-someBuiltinArg = PLC.someValue . toBuiltinArg
+someBuiltinArg = PLC.someValue . toPLCDefaultUni . toBuiltinArg
+
+-- | Create a PlutusCore 'Some' value from a 'PlyArg's data encoding form.
+someBuiltinArgData :: (PlyArg a, ToDataConstraint a) => a -> Some (ValueOf DefaultUni)
+someBuiltinArgData = PLC.someValue . toPLCDefaultUni . getDataSchemaInstanceOf . toBuiltinArgData
 
 instance PlyArg Bool where
-  type UPLCRep Bool = Bool
+  type UPLCSchema Bool = PlyBool
+  type UPLCDataSchema Bool = PlyDS ['[], '[]]
   toBuiltinArg = id
-  toBuiltinArgData = toData
+  toBuiltinArgData False = wrapDataSum $ Z Nil
+  toBuiltinArgData True = wrapDataSum . S $ Z Nil
 
 instance PlyArg Integer where
-  type UPLCRep Integer = Integer
+  type UPLCSchema Integer = PlyInt
+  type UPLCDataSchema Integer = PlyDI
   toBuiltinArg = id
-  toBuiltinArgData = toData
+  toBuiltinArgData = wrapDataInt
 
 instance PlyArg () where
-  type UPLCRep () = ()
+  type UPLCSchema () = PlyUnit
+  type UPLCDataSchema () = PlyDS '[ '[]]
   toBuiltinArg = id
-  toBuiltinArgData = toData
+  toBuiltinArgData _ = wrapDataSum $ Z Nil
 
 instance PlyArg BuiltinByteString where
-  type UPLCRep BuiltinByteString = ByteString
+  type UPLCSchema BuiltinByteString = PlyByteStr
+  type UPLCDataSchema BuiltinByteString = PlyDB
   toBuiltinArg = fromBuiltin
-  toBuiltinArgData = toData
+  toBuiltinArgData = wrapDataByteStr . PlutusTx.fromBuiltin
 
 instance PlyArg ByteString where
-  type UPLCRep ByteString = ByteString
+  type UPLCSchema ByteString = PlyByteStr
+  type UPLCDataSchema ByteString = PlyDB
   toBuiltinArg = id
-  toBuiltinArgData = toData . toBuiltin
+  toBuiltinArgData = wrapDataByteStr
 
 -- | 'toBuiltinArgData' is uncallable for this instance.
 instance PlyArg Text where
-  type UPLCRep Text = Text
-  type ToDataConstraint Text = BottomConstraint "toBuiltinArgData(Text): unsupported" Text
+  type UPLCSchema Text = PlyStr
+  type ToDataConstraint' Text = BottomConstraint "toBuiltinArgData(Text): unsupported" Text
+  type UPLCDataSchema Text = TLits.TypeError (TLits.Text "toBuiltinArgData(Text): unsupported")
   toBuiltinArg = id
   toBuiltinArgData = error "toBuiltinArgData(Text): uncallable function was called"
 
-instance PlyArg Data where
-  type UPLCRep Data = Data
-  toBuiltinArg = id
-  toBuiltinArgData = id
-
 instance (PlyArg a, PlyArg b) => PlyArg (a, b) where
-  type UPLCRep (a, b) = (UPLCRep a, UPLCRep b)
-  type ToDataConstraint (a, b) = (ToDataConstraint a, ToDataConstraint b)
+  type UPLCSchema (a, b) = PlyPairOf (UPLCSchema a) (UPLCSchema b)
+  type ToDataConstraint' (a, b) = (ToDataConstraint' a, ToDataConstraint' b)
+  type UPLCDataSchema (a, b) = PlyDS '[ [UPLCDataSchema a, UPLCDataSchema b]]
   toBuiltinArg (x, y) = (toBuiltinArg x, toBuiltinArg y)
-  toBuiltinArgData (x, y) = Constr 0 [toBuiltinArgData x, toBuiltinArgData y]
+  toBuiltinArgData (x, y) = wrapDataSum . Z $ toBuiltinArgData x :* toBuiltinArgData y :* Nil
 
 instance PlyArg a => PlyArg [a] where
-  type UPLCRep [a] = [UPLCRep a]
-  type ToDataConstraint [a] = ToDataConstraint a
+  type UPLCSchema [a] = PlyListOf (UPLCSchema a)
+  type ToDataConstraint' [a] = ToDataConstraint' a
+  type UPLCDataSchema [a] = PlyDL (UPLCDataSchema a)
   toBuiltinArg = map toBuiltinArg
-  toBuiltinArgData l = List $ map toBuiltinArgData l
+  toBuiltinArgData = wrapDataList . map toBuiltinArgData
 
 instance PlyArg PlutusTx.Rational where
-  type UPLCRep PlutusTx.Rational = Data
-  toBuiltinArg r = Constr 0 [toBuiltinArgData $ PlutusTx.numerator r, denData]
+  type UPLCDataSchema PlutusTx.Rational = PlyDS '[ [PlyDI, PlyDI]]
+  toBuiltinArgData r = wrapDataSum . Z $ denData :* denData :* Nil
     where
       den = PlutusTx.denominator r
       denData = if den /= 0 then toBuiltinArgData den else error "toBuiltinArg(PlutusTx.Rational): Expected positive denominator"
-  toBuiltinArgData = toBuiltinArg
 
-instance (PlyArg a, ToDataConstraint a) => PlyArg (Maybe a) where
-  type UPLCRep (Maybe a) = Data
-  toBuiltinArg Nothing = Constr 1 []
-  toBuiltinArg (Just x) = Constr 0 [toBuiltinArgData x]
-  toBuiltinArgData = toBuiltinArg
-
--- | This instance sorts the map.
-instance
-  ( PlyArg k
-  , ToDataConstraint k
-  , Ord k
-  , PlyArg v
-  , ToDataConstraint v
-  ) =>
-  PlyArg (PlutusMap.Map k v)
-  where
-  type UPLCRep (PlutusMap.Map k v) = [(Data, Data)]
-  toBuiltinArg =
-    map (bimap toBuiltinArgData toBuiltinArgData)
-      . M.toAscList
-      . M.fromList
-      . PlutusMap.toList
-  toBuiltinArgData = Map . toBuiltinArg
-
--- | This instance sorts the 'Value' and removes all zero entries.
-instance PlyArg Value where
-  type UPLCRep Value = [(Data, Data)]
-  toBuiltinArg (Value m) =
-    map
-      ( \(cs, tkMap) ->
-          ( toBuiltinArgData cs
-          , toBuiltinArgData
-              . PlutusMap.unsafeFromList
-              $ map (bimap toBuiltinArg toBuiltinArg) tkMap
-          )
-      )
-      . M.toAscList
-      $ M.map M.toAscList target
-    where
-      target =
-        M.filter (not . M.null . M.filter (/= 0))
-          . M.fromListWith (<>)
-          . map (second (M.fromListWith (+) . PlutusMap.toList))
-          $ PlutusMap.toList m
-  toBuiltinArgData = Map . toBuiltinArg
-
--- | This verifies the underlying bytestring is exactly 28 bytes.
-instance PlyArg Credential where
-  type UPLCRep Credential = Data
-  toBuiltinArg cred = case cred of
-    PubKeyCredential h -> Constr 0 [toBuiltinArgData h]
-    ScriptCredential h -> Constr 1 [toBuiltinArgData h]
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg StakingCredential where
-  type UPLCRep StakingCredential = Data
-  toBuiltinArg (StakingHash cred) = Constr 0 [toBuiltinArg cred]
-  toBuiltinArg x = toData x
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg Address where
-  type UPLCRep Address = Data
-  toBuiltinArg (Address cred stakeCred) =
-    let credData = toBuiltinArg cred
-        stakeCredData = toBuiltinArg stakeCred
-     in Constr 0 [credData, stakeCredData]
-  toBuiltinArgData = toBuiltinArg
-
--- | This verifies the underlying bytestring is exactly 0 (for adaSymbol) or 28 bytes.
-instance PlyArg CurrencySymbol where
-  type UPLCRep CurrencySymbol = ByteString
-  toBuiltinArg cs@(CurrencySymbol x)
-    | cs == adaSymbol = bs
-    | otherwise =
-      if BS.length bs == 28
-        then bs
-        else error "toBuiltinArg(CurrencySymbol): Expected exactly 0 or 28 bytes"
-    where
-      bs = fromBuiltin x
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
--- | This verifies the underlying bytestring is no longer than 32 bytes.
-instance PlyArg TokenName where
-  type UPLCRep TokenName = ByteString
-  toBuiltinArg (TokenName x) =
-    if BS.length bs <= 32
-      then bs
-      else error "toBuiltinArg(TokenName): Expected 32 bytes or less"
-    where
-      bs = fromBuiltin x
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
-instance PlyArg AssetClass where
-  type UPLCRep AssetClass = Data
-  toBuiltinArg (AssetClass i) = toBuiltinArgData i
-  toBuiltinArgData = toBuiltinArg
-
--- | This verifies the underlying bytestring is exactly 28 bytes.
-instance PlyArg PubKeyHash where
-  type UPLCRep PubKeyHash = ByteString
-  toBuiltinArg (PubKeyHash x) =
-    if BS.length bs == 28
-      then bs
-      else error "toBuiltinArg(PubKeyHash): Expected 28 bytes"
-    where
-      bs = fromBuiltin x
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
--- | This verifies the underlying integer is non-negative.
-instance PlyArg POSIXTime where
-  type UPLCRep POSIXTime = Integer
-  toBuiltinArg (POSIXTime i) =
-    if i >= 0
-      then i
-      else error "toBuiltinArg(POSIXTime): Expected non-negative"
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
--- | This verifies the underlying integer is non-negative.
-instance PlyArg DiffMilliSeconds where
-  type UPLCRep DiffMilliSeconds = Integer
-  toBuiltinArg (DiffMilliSeconds i) =
-    if i >= 0
-      then i
-      else error "toBuiltinArg(DiffMilliSeconds): Expected non-negative"
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
-instance (PlyArg a, ToDataConstraint a) => PlyArg (Extended a) where
-  type UPLCRep (Extended a) = Data
-  toBuiltinArg NegInf = Constr 0 []
-  toBuiltinArg (Finite a) = Constr 1 [toBuiltinArgData a]
-  toBuiltinArg PosInf = Constr 2 []
-  toBuiltinArgData = toBuiltinArg
-
-instance (PlyArg a, ToDataConstraint a) => PlyArg (UpperBound a) where
-  type UPLCRep (UpperBound a) = Data
-  toBuiltinArg (UpperBound ext cls) = Constr 0 [toBuiltinArgData ext, toBuiltinArgData cls]
-  toBuiltinArgData = toBuiltinArg
-
-instance (PlyArg a, ToDataConstraint a) => PlyArg (LowerBound a) where
-  type UPLCRep (LowerBound a) = Data
-  toBuiltinArg (LowerBound ext cls) = Constr 0 [toBuiltinArgData ext, toBuiltinArgData cls]
-  toBuiltinArgData = toBuiltinArg
-
-instance (PlyArg a, ToDataConstraint a) => PlyArg (Interval a) where
-  type UPLCRep (Interval a) = Data
-  toBuiltinArg (Interval lb ub) = Constr 0 [toBuiltinArgData lb, toBuiltinArgData ub]
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg DCert where
-  type UPLCRep DCert = Data
-  toBuiltinArg (DCertDelegRegKey ext) = Constr 0 [toBuiltinArgData ext]
-  toBuiltinArg (DCertDelegDeRegKey ext) = Constr 1 [toBuiltinArgData ext]
-  toBuiltinArg (DCertDelegDelegate ext pkh) = Constr 2 [toBuiltinArgData ext, toBuiltinArgData pkh]
-  toBuiltinArg (DCertPoolRegister pkh1 pkh2) = Constr 3 [toBuiltinArgData pkh1, toBuiltinArgData pkh2]
-  toBuiltinArg (DCertPoolRetire pkh i) = Constr 4 [toBuiltinArgData pkh, toBuiltinArgData i]
-  toBuiltinArg DCertGenesis = Constr 5 []
-  toBuiltinArg DCertMir = Constr 6 []
-  toBuiltinArgData = toBuiltinArg
-
--- | This verifies the underlying bytestring is exactly 32 bytes.
-instance PlyArg TxId where
-  -- TxId is a Constr data instead of just a bytestring, for some reason.
-  type UPLCRep TxId = Data
-  toBuiltinArg (TxId x) =
-    if BS.length bs == 32
-      then Constr 0 [toBuiltinArgData bs]
-      else error "toBuiltinArg(TxId): Expected 32 bytes"
-    where
-      bs = fromBuiltin x
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
--- | This verifies the txIdx is non-negative.
-instance PlyArg TxOutRef where
-  type UPLCRep TxOutRef = Data
-  toBuiltinArg (TxOutRef txId txIdx) =
-    if txIdx >= 0
-      then Constr 0 [toBuiltinArgData txId, toBuiltinArgData txIdx]
-      else error "toBuiltinArg(TxOutRef): Expected non-negative idx"
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg LedgerV1.TxOut where
-  type UPLCRep LedgerV1.TxOut = Data
-  toBuiltinArg (LedgerV1.TxOut addr val dh) =
-    Constr
-      0
-      [ toBuiltinArgData addr
-      , toBuiltinArgData val
-      , toBuiltinArgData dh
-      ]
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg LedgerV1.TxInInfo where
-  type UPLCRep LedgerV1.TxInInfo = Data
-  toBuiltinArg (LedgerV1.TxInInfo ref out) = Constr 0 [toBuiltinArgData ref, toBuiltinArgData out]
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg LedgerV1.TxInfo where
-  type UPLCRep LedgerV1.TxInfo = Data
-  toBuiltinArg
-    ( LedgerV1.TxInfo
-        inps
-        outs
-        fee
-        mint
-        dcert
-        wdrl
-        rnge
-        signatories
-        datumMap
-        txId
-      ) =
-      Constr
-        0
-        [ toBuiltinArgData inps
-        , toBuiltinArgData outs
-        , toBuiltinArgData fee
-        , toBuiltinArgData mint
-        , toBuiltinArgData dcert
-        , toBuiltinArgData wdrl
-        , toBuiltinArgData rnge
-        , toBuiltinArgData signatories
-        , toBuiltinArgData datumMap
-        , toBuiltinArgData txId
-        ]
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg ScriptPurpose where
-  type UPLCRep ScriptPurpose = Data
-  toBuiltinArg (Minting cs) = Constr 0 [toBuiltinArgData cs]
-  toBuiltinArg (Spending ref) = Constr 1 [toBuiltinArgData ref]
-  toBuiltinArg (Rewarding stakeCred) = Constr 2 [toBuiltinArgData stakeCred]
-  toBuiltinArg (Certifying dcert) = Constr 3 [toBuiltinArgData dcert]
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg LedgerV1.ScriptContext where
-  type UPLCRep LedgerV1.ScriptContext = Data
-  toBuiltinArg (LedgerV1.ScriptContext inf purp) = Constr 0 [toBuiltinArgData inf, toBuiltinArgData purp]
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg Datum where
-  type UPLCRep Datum = Data
-  toBuiltinArg (Datum d) = toData d
-  toBuiltinArgData = toBuiltinArg
-
-instance PlyArg Redeemer where
-  type UPLCRep Redeemer = Data
-  toBuiltinArg (Redeemer d) = toData d
-  toBuiltinArgData = toBuiltinArg
-
--- | This verifies the underlying bytestring is exactly 28 bytes.
-instance PlyArg ScriptHash where
-  type UPLCRep ScriptHash = ByteString
-  toBuiltinArg (ScriptHash x) =
-    if BS.length bs == 28
-      then bs
-      else error "toBuiltinArg(ScriptHash): Expected 28 bytes"
-    where
-      bs = fromBuiltin x
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
--- | This verifies the underlying bytestring is exactly 28 bytes.
-instance PlyArg DatumHash where
-  type UPLCRep DatumHash = ByteString
-  toBuiltinArg (DatumHash x) =
-    if BS.length bs == 28
-      then bs
-      else error "toBuiltinArg(DatumHash): Expected 28 bytes"
-    where
-      bs = fromBuiltin x
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
--- | This verifies the underlying bytestring is exactly 28 bytes.
-instance PlyArg RedeemerHash where
-  type UPLCRep RedeemerHash = ByteString
-  toBuiltinArg (RedeemerHash x) =
-    if BS.length bs == 28
-      then bs
-      else error "toBuiltinArg(RedeemerHash): Expected 28 bytes"
-    where
-      bs = fromBuiltin x
-  toBuiltinArgData = toBuiltinArgData . toBuiltinArg
-
-instance (PlyArg a, ToDataConstraint a) => PlyArg (AsData a) where
-  type UPLCRep (AsData a) = Data
-  toBuiltinArg (AsData x) = toBuiltinArgData x
-  toBuiltinArgData = toBuiltinArg
+instance (PlyArg a, ToDataConstraint a, HasSchemaInstance (PlyD (UPLCDataSchema a))) => PlyArg (Maybe a) where
+  type UPLCDataSchema (Maybe a) = PlyDS '[ '[UPLCDataSchema a], '[]]
+  toBuiltinArgData Nothing = wrapDataSum . S $ Z Nil
+  toBuiltinArgData (Just x) = wrapDataSum . Z $ toBuiltinArgData x :* Nil
---
+++ ply-core/src/Ply/Core/Schema.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+
+module Ply.Core.Schema (
+  PlySchema (..),
+  PlyDataSchema (..),
+  SchemaDescription (..),
+  HasSchemaInstance (..),
+  DataSchemaInstanceOf (..),
+  ToPLCDefaultUni (..),
+  wrapDataInt,
+  wrapDataByteStr,
+  wrapDataList,
+  wrapDataSum,
+  schemaDescrOf,
+) where
+
+import Data.Bifunctor (bimap)
+import Data.ByteString (ByteString)
+import Data.Kind (Constraint, Type)
+import Data.Text (Text)
+
+import Data.SOP (All, All2, Compose, NS (..), Proxy (Proxy), SOP (..))
+import Data.SOP.BasicFunctors (K (K))
+import Data.SOP.NP (NP, cmap_NP, collapse_NP)
+import Data.SOP.NS (ccata_NS, index_NS)
+
+import qualified PlutusCore as PLC
+import qualified PlutusCore.Data as PlutusData
+import qualified PlutusLedgerApi.V1 as PlutusTx
+import qualified PlutusTx.IsData as PlutusData
+
+data PlySchema
+  = PlyInt
+  | PlyByteStr
+  | PlyStr
+  | PlyUnit
+  | PlyBool
+  | PlyListOf PlySchema
+  | PlyPairOf PlySchema PlySchema
+  | PlyD PlyDataSchema
+
+data PlyDataSchema
+  = PlyDS [[PlyDataSchema]]
+  | PlyDM PlyDataSchema PlyDataSchema
+  | PlyDL PlyDataSchema
+  | PlyDI
+  | PlyDB
+
+data SchemaDescription
+  = SimpleType String
+  | ListType SchemaDescription
+  | PairType SchemaDescription SchemaDescription
+  | DataListType SchemaDescription
+  | MapType SchemaDescription SchemaDescription
+  | ConstrType [[SchemaDescription]]
+
+schemaDescrOf :: PlySchema -> SchemaDescription
+schemaDescrOf PlyInt = SimpleType "#integer"
+schemaDescrOf PlyByteStr = SimpleType "#bytes"
+schemaDescrOf PlyStr = SimpleType "#string"
+schemaDescrOf PlyUnit = SimpleType "#unit"
+schemaDescrOf PlyBool = SimpleType "#boolean"
+schemaDescrOf (PlyListOf a) = ListType $ schemaDescrOf a
+schemaDescrOf (PlyPairOf a b) = PairType (schemaDescrOf a) (schemaDescrOf b)
+schemaDescrOf (PlyD (PlyDS xss)) = ConstrType $ map (schemaDescrOf . PlyD) <$> xss
+schemaDescrOf (PlyD (PlyDM a b)) = MapType (schemaDescrOf $ PlyD a) (schemaDescrOf $ PlyD b)
+schemaDescrOf (PlyD (PlyDL a)) = DataListType $ schemaDescrOf (PlyD a)
+schemaDescrOf (PlyD PlyDI) = SimpleType "integer"
+schemaDescrOf (PlyD PlyDB) = SimpleType "bytes"
+
+newtype DataInt = DataInt Integer
+
+newtype DataByteStr = DataByteStr ByteString
+
+type DataList :: PlyDataSchema -> Type
+newtype DataList a = DataList [DataSchemaInstanceOf a]
+
+type DataSum :: [[PlyDataSchema]] -> Type
+newtype DataSum xss = DataSum (SOP DataSchemaInstanceOf xss)
+
+data DataSchemaInstanceOf a = DataSchemaInstanceOf {getDataSchemaInstanceOf :: SchemaInstanceOf (PlyD a)}
+
+instance (ToPLCDefaultUni (PlyD a), DefaultUniTarget (PlyD a) ~ PlutusData.Data) => PlutusData.ToData (DataSchemaInstanceOf a) where
+  toBuiltinData = PlutusTx.BuiltinData . toPLCDefaultUni . getDataSchemaInstanceOf
+
+wrapDataInt :: Integer -> DataSchemaInstanceOf 'PlyDI
+wrapDataInt = DataSchemaInstanceOf . DataInt
+
+wrapDataByteStr :: ByteString -> DataSchemaInstanceOf 'PlyDB
+wrapDataByteStr = DataSchemaInstanceOf . DataByteStr
+
+wrapDataList :: [DataSchemaInstanceOf a] -> DataSchemaInstanceOf ( 'PlyDL a)
+wrapDataList = DataSchemaInstanceOf . DataList
+
+wrapDataSum :: NS (NP DataSchemaInstanceOf) xss -> DataSchemaInstanceOf ( 'PlyDS xss)
+wrapDataSum = DataSchemaInstanceOf . DataSum . SOP
+
+type HasSchemaInstance :: PlySchema -> Constraint
+class HasSchemaInstance a where
+  type SchemaInstanceOf a = (f :: Type) | f -> a
+
+instance HasSchemaInstance PlyInt where
+  type SchemaInstanceOf PlyInt = Integer
+
+instance HasSchemaInstance PlyByteStr where
+  type SchemaInstanceOf PlyByteStr = ByteString
+
+instance HasSchemaInstance PlyStr where
+  type SchemaInstanceOf PlyStr = Text
+
+instance HasSchemaInstance PlyUnit where
+  type SchemaInstanceOf PlyUnit = ()
+
+instance HasSchemaInstance PlyBool where
+  type SchemaInstanceOf PlyBool = Bool
+
+instance HasSchemaInstance a => HasSchemaInstance (PlyListOf a) where
+  type SchemaInstanceOf (PlyListOf a) = [SchemaInstanceOf a]
+
+instance (HasSchemaInstance a, HasSchemaInstance b) => HasSchemaInstance (PlyPairOf a b) where
+  type SchemaInstanceOf (PlyPairOf a b) = (SchemaInstanceOf a, SchemaInstanceOf b)
+
+instance HasSchemaInstance (PlyD PlyDI) where
+  type SchemaInstanceOf (PlyD PlyDI) = DataInt
+
+instance HasSchemaInstance (PlyD PlyDB) where
+  type SchemaInstanceOf (PlyD PlyDB) = DataByteStr
+
+instance HasSchemaInstance (PlyD a) => HasSchemaInstance (PlyD (PlyDL a)) where
+  type SchemaInstanceOf (PlyD (PlyDL a)) = DataList a
+
+instance All2 (Compose HasSchemaInstance PlyD) a => HasSchemaInstance (PlyD (PlyDS a)) where
+  type SchemaInstanceOf (PlyD (PlyDS a)) = DataSum a
+
+type ToPLCDefaultUni :: PlySchema -> Constraint
+class PLC.DefaultUni `PLC.HasTermLevel` DefaultUniTarget a => ToPLCDefaultUni a where
+  type DefaultUniTarget a :: Type
+  type DefaultUniTarget a = SchemaInstanceOf a
+  toPLCDefaultUni :: SchemaInstanceOf a -> DefaultUniTarget a
+
+instance ToPLCDefaultUni PlyInt where
+  toPLCDefaultUni = id
+
+instance ToPLCDefaultUni PlyByteStr where
+  toPLCDefaultUni = id
+
+instance ToPLCDefaultUni PlyStr where
+  toPLCDefaultUni = id
+
+instance ToPLCDefaultUni PlyUnit where
+  toPLCDefaultUni = id
+
+instance ToPLCDefaultUni a => ToPLCDefaultUni (PlyListOf a) where
+  type DefaultUniTarget (PlyListOf a) = [DefaultUniTarget a]
+  toPLCDefaultUni = map toPLCDefaultUni
+
+instance (ToPLCDefaultUni a, ToPLCDefaultUni b) => ToPLCDefaultUni (PlyPairOf a b) where
+  type DefaultUniTarget (PlyPairOf a b) = (DefaultUniTarget a, DefaultUniTarget b)
+  toPLCDefaultUni = bimap toPLCDefaultUni toPLCDefaultUni
+
+instance ToPLCDefaultUni PlyBool where
+  toPLCDefaultUni = id
+
+instance ToPLCDefaultUni (PlyD PlyDI) where
+  type DefaultUniTarget (PlyD PlyDI) = PlutusData.Data
+  toPLCDefaultUni (DataInt x) = PlutusData.I x
+
+instance ToPLCDefaultUni (PlyD PlyDB) where
+  type DefaultUniTarget (PlyD PlyDB) = PlutusData.Data
+  toPLCDefaultUni (DataByteStr x) = PlutusData.B x
+
+instance (ToPLCDefaultUni (PlyD a), DefaultUniTarget (PlyD a) ~ PlutusData.Data) => ToPLCDefaultUni (PlyD (PlyDL a)) where
+  type DefaultUniTarget (PlyD (PlyDL a)) = PlutusData.Data
+  toPLCDefaultUni (DataList x) = PlutusData.List $ map (toPLCDefaultUni . getDataSchemaInstanceOf) x
+
+instance All2 (Compose PlutusData.ToData DataSchemaInstanceOf) xss => ToPLCDefaultUni (PlyD (PlyDS xss)) where
+  type DefaultUniTarget (PlyD (PlyDS xss)) = PlutusData.Data
+  toPLCDefaultUni (DataSum (SOP nsnp)) = PlutusData.Constr (toInteger $ index_NS nsnp) dataFields
+    where
+      K dataFields =
+        ccata_NS
+          (Proxy @(All (Compose PlutusData.ToData DataSchemaInstanceOf)))
+          ( K
+              . collapse_NP
+              . cmap_NP
+                (Proxy @(Compose PlutusData.ToData DataSchemaInstanceOf))
+                (K . PlutusData.toData)
+          )
+          (\(K x) -> K x)
+          nsnp
