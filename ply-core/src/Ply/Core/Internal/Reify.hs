250ca9daf836f3232e2db92dd61fc3a89f8bf408
TotallyNotChase <44284917+TotallyNotChase@users.noreply.github.com>
2025-03-09 17:25:44 -0600

    Update `ReifySchemas` and remove `ReifyVersion`

--- ply-core/src/Ply/Core/Internal/Reify.hs
+++ ply-core/src/Ply/Core/Internal/Reify.hs
@@ -1,39 +1,22 @@
 {-# LANGUAGE UndecidableInstances #-}
 
-module Ply.Core.Internal.Reify (ReifyVersion (reifyVersion), ReifyRole (reifyRole), ReifySchemas (reifySchemas)) where
+module Ply.Core.Internal.Reify (ReifyRole (reifyRole), ReifySchemas (reifySchemas)) where
 
 import Data.Kind (Constraint, Type)
 import Data.Proxy (Proxy (Proxy))
 
-import Ply.Core.Class (PlyArg (UPLCDataSchema), ToDataConstraint)
-import Ply.Core.Schema.Description (
-  HasDataSchemaDescription,
-  schemaDescrOf',
- )
-import Ply.Core.Schema.Types (PlySchema (PlyD))
+import PlutusTx.Blueprint (HasBlueprintSchema (schema), Schema)
 import Ply.Core.Types (
-  SchemaDescription,
   ScriptRole (MintingPolicyRole, ValidatorRole),
-  ScriptVersion (ScriptV1, ScriptV2),
  )
-
-type ReifyVersion :: ScriptVersion -> Constraint
-class ReifyVersion s where
-  reifyVersion :: Proxy s -> ScriptVersion
 
 type ReifyRole :: ScriptRole -> Constraint
 class ReifyRole s where
   reifyRole :: Proxy s -> ScriptRole
 
-type ReifySchemas :: [Type] -> Constraint
-class ReifySchemas ts where
-  reifySchemas :: Proxy ts -> [SchemaDescription]
-
-instance ReifyVersion ScriptV1 where
-  reifyVersion _ = ScriptV1
-
-instance ReifyVersion ScriptV2 where
-  reifyVersion _ = ScriptV2
+type ReifySchemas :: [Type] -> [Type] -> Constraint
+class ReifySchemas referencedTypes ts where
+  reifySchemas :: Proxy referencedTypes -> Proxy ts -> [Schema referencedTypes]
 
 instance ReifyRole ValidatorRole where
   reifyRole _ = ValidatorRole
@@ -41,8 +24,8 @@ instance ReifyRole ValidatorRole where
 instance ReifyRole MintingPolicyRole where
   reifyRole _ = MintingPolicyRole
 
-instance ReifySchemas '[] where
-  reifySchemas _ = []
+instance ReifySchemas referencedTypes '[] where
+  reifySchemas _ _ = []
 
-instance (PlyArg x, ToDataConstraint x, HasDataSchemaDescription (UPLCDataSchema x), ReifySchemas xs) => ReifySchemas (x : xs) where
-  reifySchemas _ = schemaDescrOf' @(PlyD (UPLCDataSchema x)) : reifySchemas (Proxy @xs)
+instance (HasBlueprintSchema x referencedTypes, ReifySchemas referencedTypes xs) => ReifySchemas referencedTypes (x : xs) where
+  reifySchemas _ _ = schema @x : reifySchemas (Proxy @referencedTypes) (Proxy @xs)
